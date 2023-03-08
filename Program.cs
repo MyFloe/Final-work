8cd7713d60758fb8428f83768035342c8e06acbd
Floe <i.a.spiridonova@gmail.com>
2023-03-08 23:15:18 +0300

    Добавлен Program.cs изменения в readme

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите массив данных через ',': ");
+string strLine = Console.ReadLine();
+string[] firstArr = strLine.Split(",");
+string[] secondArr = new string[0];
+
+for (int i = 0; i < firstArr.Length; i++)
+{
+    if (firstArr[i].Length <= 3)
+    {
+        Array.Resize(ref secondArr, secondArr.Length + 1);
+        secondArr[secondArr.Length-1] = firstArr[i];
+    }
+
+}
+Console.WriteLine($"[{string.Join(",", firstArr)}]=>[{string.Join(",", secondArr)}]");
